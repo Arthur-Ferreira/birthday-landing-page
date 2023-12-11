77c9076581222a8e391fe4eb5ef33b16da02a3f3
Arthur Ferreira <63617527+Arthur-Ferreira@users.noreply.github.com>
2023-12-11 02:31:35 -0300

    Project finish setup

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,8 +1,18 @@
 const mobileMenuBtnElement = document.getElementById('mobile-menu-btn');
 const mobileMenuElement = document.getElementById('mobile-menu');
+const navItems = document.querySelectorAll('a.mobile-menu__nav__items__item--link');
 
 function toggleMobileMenu() {
   mobileMenuElement.classList.toggle('open');
 }
 
+function closeMobileMenu() {
+  mobileMenuElement.classList.remove('open');
+}
+
+
 mobileMenuBtnElement.addEventListener('click', toggleMobileMenu);
+
+navItems.forEach(element => {
+  element.addEventListener('click', closeMobileMenu);
+});
